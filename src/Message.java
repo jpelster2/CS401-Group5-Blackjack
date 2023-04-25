bc71bd9f53aa2388178dae224ee33a9e0c3488f5
Anas1670 <78567580+Anas1670@users.noreply.github.com>
2023-04-24 17:31:03 -0700

    Added separate getters for each function and removed getMessage.

--- src/Message.java
+++ src/Message.java
@@ -12,10 +12,20 @@ public class Message {
     }
 
     public void Messageprint() {
-        System.out.println ("Message sent: " + this.text);
+        System.out.println ("Message Sent: " + this.text);
+        System.out.println ("Special Data: " + this.specialData);
+        System.out.println ("Action: " + this.action);
     }
-
-    public String getMessage() {
-        return this.text + this.specialData + this.action;
+    
+    public String getText() {
+        return this.text;
+    }
+    
+    public String getspecialData() {
+        return this.specialData;
+    }
+    
+    public String getAction() {
+        return this.action;
     }
 }
