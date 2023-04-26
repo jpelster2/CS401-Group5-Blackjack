6084c9f9aaa65afeeecf69225f212aa060301fde
graysonman <graysontvhull@gmail.com>
2023-04-26 13:36:18 -0700

    Create GUI.java
    Created a gui class specifically for the game as the client displays lobby in joptionpane.

---
+++ src/GUI.java
@@ -0,0 +1,79 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI {
+
+    private JFrame frame;
+    private JPanel panel;
+    private JLabel playerHandLabel;
+    private JLabel dealerHandLabel;
+    private JLabel deckLabel;
+    private JLabel statusLabel;
+    private JButton hitButton;
+    private JButton standButton;
+    private JButton leaveGameButton;
+
+
+    public GUI() {
+        initialize();
+    }
+
+    private void initialize() {
+        frame = new JFrame("Blackjack");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+
+        panel = new JPanel();
+        frame.add(panel);
+        panel.setLayout(new BorderLayout());
+
+        JPanel topPanel = new JPanel();
+        panel.add(topPanel, BorderLayout.NORTH);
+        topPanel.setLayout(new GridLayout(2, 1));
+
+        dealerHandLabel = new JLabel("Dealer's hand: ");
+        topPanel.add(dealerHandLabel);
+        playerHandLabel = new JLabel("Player's hand: ");
+        topPanel.add(playerHandLabel);
+
+        deckLabel = new JLabel("Deck: ");
+        panel.add(deckLabel, BorderLayout.CENTER);
+
+        statusLabel = new JLabel("Status: ");
+        panel.add(statusLabel, BorderLayout.SOUTH);
+
+        JPanel buttonPanel = new JPanel();
+        panel.add(buttonPanel, BorderLayout.EAST);
+        buttonPanel.setLayout(new GridLayout(2, 1));
+
+        hitButton = new JButton("Hit");
+        buttonPanel.add(hitButton);
+        hitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	playerHandLabel.setText("Player's hand: 10 of Hearts");
+            }
+        });
+
+        standButton = new JButton("Stand");
+        buttonPanel.add(standButton);
+        standButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Implement stand action
+            }
+        });
+        
+        leaveGameButton = new JButton("Leave Game");
+        buttonPanel.add(leaveGameButton);
+        leaveGameButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                //lobby();
+            }
+        });
+    }
+}
