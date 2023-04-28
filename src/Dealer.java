3795762aa6597c4e1441b462805a409116a4f8d9
Desktop <vtran103@horizon.csueastbay.edu>
2023-04-28 16:57:43 -0700

    Created Dealer class

---
+++ src/Dealer.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Dealer {
+	private ArrayList<Card> deck;
+	private ArrayList<Card> hand;
+	
+	public Dealer() {
+		
+		
+	}
+	
+	public void reset() {
+		
+	}
+	public void shuffle() {
+		
+	}
+	
+}
