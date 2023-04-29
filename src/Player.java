f9ce7888e6090d6c6aeae51ac92a5a97f3b4420f
uorph <rlillyrichardson@gmail.com>
2023-04-29 14:46:08 -0700

    Updates to the Player Class
    - Added currentScore()
    - Added emptyHand()
    - added addCardToHand(Card) 
    - added removeFunds(float)

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,5 @@
-import java.util.*;
+import java.util.ArrayList;
+
 public class Player {
 	private String id; 
 	private String username; 
@@ -12,11 +13,16 @@ public class Player {
 		setId(username);
 		setPassword(password);
 		setBalance(balance);
+		ArrayList<Card> hand = new ArrayList<Card>(); 
+		setHand(hand);
 	}
 	public Player() {
 	}
 	public void setId(String id) {
 		this.id = id;
+	}
+	public void setHand(ArrayList<Card>hand) {
+		this.hand = hand;
 	}
 	public void setUsername(String username) {
 		this.username = username;
@@ -45,23 +51,46 @@ public class Player {
 	public float getCurrentBet() {
 		return currentBet;
 	}
+	public ArrayList<Card> getHand() {
+		return hand; 
+	}
 	public void addFunds(float funds) {
 		float newBalance = getBalance() + funds; 
 		setBalance(newBalance); 
+	}
+	public void removeFunds(float funds) {
+		float newBalance = getBalance() - funds;
+		setBalance(newBalance);
 	}
 	public void increaseBet(float amount) {
 		float newBet = getCurrentBet() + amount; 
 		setCurrentBet(newBet); 
 	}
-	/*
-	public void requestCard() {
-		
+	public void addCardToHand(Card card){
+		ArrayList<Card> hand = getHand(); 
+		hand.add(card);
 	}
 	public void emptyHand() {
-		
+		ArrayList<Card>hand = new ArrayList<Card>(); 
+		setHand(hand); 
 	}
 	public int currentScore() {
-		
+		// Cards 2-10 are worth their face value 
+		// Face cards jack, queen, king are worth 10 
+		int score = 0;
+		boolean hasAce = false; 
+		ArrayList<Card>hand = getHand(); 
+		for(int i = 0; i < hand.size(); i++) {
+			int holding = hand.get(i).getValue(); 
+			score+=holding; 
+			if(holding == 11) {
+				hasAce = true;
+			}
+		}
+		if((score > 21) && (hasAce == true)) {
+			score = score - 10; 
+			System.out.println("The total score is above 21 therefore the value of Ace has been changed from 11 to 1");
+		}
+		return score;
 	}
-	*/
 }
