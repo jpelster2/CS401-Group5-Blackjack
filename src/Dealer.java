3870d79a104cd5d43e4da966dcf1c5d356867a27
Desktop <vtran103@horizon.csueastbay.edu>
2023-04-28 20:16:41 -0700

    Update Dealer.java

--- src/Dealer.java
+++ src/Dealer.java
@@ -9,7 +9,21 @@ public class Dealer {
 	}
 	
 	public void reset() {
-		//fill a new deck, loop by suit and # of decks
+		deck.clear();//throw away current deck
+		emptyHand();//throw away current hand
+		//refill a new deck, loop by suit and # of decks
+		int numDecks = 3;
+		for (int i = 0; i< numDecks; i++) {//add one deck per loop
+			CardSuit Suit = CardSuit.SPADES;
+			addSuit(Suit);
+			Suit = CardSuit.CLUBS;
+			addSuit(Suit);
+			Suit = CardSuit.HEARTS;
+			addSuit(Suit);
+			Suit = CardSuit.DIAMONDS;
+			addSuit(Suit);
+		}
+
 		
 	}
 	public void shuffle() {
@@ -30,9 +44,19 @@ public class Dealer {
 		}
 		return total;
 	}
+	public void drawCard() {
+		Card card = new Card(0, "placeholder", CardSuit.CLUBS);//should eventually draw from deck
+		hand.add(card);
+	}
 	public void emptyHand() {
 		//discard hand
 		hand.clear();
+	}
+	
+	private void addSuit(CardSuit Suit) {//helper
+		//makes numbered cards 2-10
+		
+		//makes face cards and aces
 	}
 	
 }
