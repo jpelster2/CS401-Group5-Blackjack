a4db456d82010205096b607bcca9affb1e0308dd
Desktop <vtran103@horizon.csueastbay.edu>
2023-04-28 17:00:31 -0700

    Update Dealer.java

--- src/Dealer.java
+++ src/Dealer.java
@@ -4,15 +4,15 @@ public class Dealer {
 	private ArrayList<Card> hand;
 	
 	public Dealer() {
-		
+		//call reset to get a fresh deck
 		
 	}
 	
 	public void reset() {
-		
+		//fill a new deck
 	}
 	public void shuffle() {
-		
+		//shuffle remaining cards
 	}
 	
 }
