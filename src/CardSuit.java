7cad03b0a9c29f01d0bbca8dc6349af009511704
Desktop <vtran103@horizon.csueastbay.edu>
2023-04-28 20:49:17 -0700

    Update CardSuit.java

--- src/CardSuit.java
+++ src/CardSuit.java
@@ -3,5 +3,21 @@ public enum CardSuit {
 	SPADES,
 	CLUBS,
 	HEARTS,
-	DIAMONDS
+	DIAMONDS,
+	;
+	@Override
+	public String toString() {//for using stringbuilder with card names
+	    switch (this) {
+	        case SPADES:
+	            return "SPADES";
+	        case CLUBS:
+	            return "CLUBS";
+	        case HEARTS:
+	            return "HEARTS";
+	        case DIAMONDS:
+	            return "DIAMONDS";
+	        default:
+	            return null;
+	    }
+	}
 }
